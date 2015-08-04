dee155becebe8e47f1282d895c18b53ff798cdaf
cahowar3 <cahowar3@ncsu.edu>
2015-08-04 19:36:52 -0400

    Practice

--- BlackJack/src/blackJack/BlackJackMain.java
+++ BlackJack/src/blackJack/BlackJackMain.java
@@ -9,6 +9,19 @@ public class BlackJackMain {
 	public static Hand dealerHand = new Hand();
 	public static Scanner console = new Scanner(System.in);
 	
+	private static boolean checkBlackJack() {
+		if (myHand.returnValue() == 21) {
+			System.out.println("Player Wins!");
+			return true;
+		}
+		
+		else if (dealerHand.returnValue() == 21) {
+			System.out.println("Dealer Wins!");
+			return true;
+		}
+		return false;
+		
+	}
 	private static void stand() {
 		while (dealerHand.returnValue() < 17) {
 			dealerHand.addCard(myDeck.dealCard());
@@ -23,26 +36,33 @@ public class BlackJackMain {
 	private void hit() {
 		
 	}
-	private static void checkWinner() {
-		if (myHand.returnValue() == 21) {
-			System.out.println("Player Wins!");
-		}
-		
-		else if (dealerHand.returnValue() == 21) {
-			System.out.println("Dealer Wins!");
-		}
-		
-		else if (myHand.returnValue() > 21) {
+	private static boolean checkWinner() {
+		if (myHand.returnValue() > 21) {
 			System.out.println("Player Busts!");
+			return true;
 		}
 		
 		else if (dealerHand.returnValue() > 21) {
 			System.out.println("Dealer Busts!");
+			return true;
 		}
 		
 		else if (dealerHand.returnValue() == myHand.returnValue()) {
 			System.out.println("Push");
+			return true;
 		}
+		
+		else if (dealerHand.returnValue() > myHand.returnValue()) {
+			System.out.println("Dealer Wins!");
+			return true;
+		}
+		
+		else if (myHand.returnValue() > dealerHand.returnValue()) {
+			System.out.println("Player Wins!");
+			return true;
+		}
+		
+		return false;
 	}
 	
 	public static void main(String[] args) {
@@ -60,7 +80,9 @@ public class BlackJackMain {
 		System.out.println("Dealer's Hand.");
 		dealerHand.printCards();
 		System.out.println(dealerHand.returnValue());
-		checkWinner();
+		
+		checkBlackJack();
+				
 		System.out.println("Hit or Stand? H/S");
 		
 		if (console.next().equalsIgnoreCase("S")) {
